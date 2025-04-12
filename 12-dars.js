355c6bc266a35c3605bdbf0596e48537a69a32c6
rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-04-12 13:37:04 +0500

    array start

---
+++ 12-dars.js
@@ -0,0 +1,17 @@
+//Array
+// const arr = ['olma', 'anor', 'nok', 12, 1, 2, 3, 'London', 'ewe']
+
+// let arr1 = new Array(1, 2, 3, 4, 5)
+// console.log(arr1[0])    ikkinchi usul
+
+// console.log(arr[arr.length - 1])   //ixirgi qiymatni olish
+
+
+let numbers = [1, 2, 3, 4]
+numbers.push(5)    //arrayga malumot qo'shish
+numbers[0] = 123 //yangilash
+numbers.pop()  //exirgi elemntni ochiradi
+numbers.shift()  //boshidagi elemntni ichiradi
+
+numbers.unshift(1234)   //boshiga malumot qoshadi
+console.log(numbers)
