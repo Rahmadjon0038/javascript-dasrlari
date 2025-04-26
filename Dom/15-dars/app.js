01d675da7882010d367d012ad4c99cbee00207ac
rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-04-26 14:08:21 +0500

    15-dars

--- Dom/15-dars/app.js
+++ Dom/15-dars/app.js
@@ -1,51 +1,53 @@
-// document
-// window  =>  javascripdagi eng katta object
+// // document
+// // window  =>  javascripdagi eng katta object
 
-// const ul = document.getElementsByTagName('ul')
-// const li = document.getElementsByTagName('li')
-// const title = document.getElementsByTagName('h1')
+// // const ul = document.getElementsByTagName('ul')
+// // const li = document.getElementsByTagName('li')
+// // const title = document.getElementsByTagName('h1')
 
-// console.log(ul)
-// console.log(li)
+// // console.log(ul)
+// // console.log(li)
 
-// selecttors
-// function addclass(soz) {
-//     title.className += ' yangi'
-//     title.textContent = soz
-// }
-// console.log(title)
+// // selecttors
+// // function addclass(soz) {
+// //     title.className += ' yangi'
+// //     title.textContent = soz
+// // }
+// // console.log(title)
 
-// box.innerHTML += `<h1>Yangi teg</h1>`
-// console.log(box)
+// // box.innerHTML += `<h1>Yangi teg</h1>`
+// // console.log(box)
 
 
-const title = document.querySelector('.title')
-const box = document.getElementById('box')
-const addTeg = document.getElementById('addTeg')
+// const title = document.querySelector('.title')
+// const box = document.getElementById('box')
+// const addTeg = document.getElementById('addTeg')
 
-title.textContent += ' sa'     //matnni uzgartirish
-title.innerHTML = '<i>salom</i> <p class="matn">nima gapla</p>'   //html element qoshish
-title.className += ' yangi'   //class qoshish
+// title.textContent += ' sa'     //matnni uzgartirish
+// title.innerHTML = '<i>salom</i> <p class="matn">nima gapla</p>'   //html element qoshish
+// title.className += ' yangi'   //class qoshish
 
-addTeg.addEventListener('click', () => {
-    title.className = 'title'
-})
-
-// document.addEventListener('keydown', (key) => {
-// title.textContent = key.key
+// addTeg.addEventListener('click', () => {
+//     title.className = 'title'
 // })
 
-
-const change = document.getElementById('change')
-
-const box1 = document.getElementById('box1')
-
-// box1.classList.add('text')  //class qoshish
-// box1.classList.remove('text')   //classni ochirish
+// // document.addEventListener('keydown', (key) => {
+// // title.textContent = key.key
+// // })
 
 
-box1.addEventListener('click', () => {
-    box1.classList.toggle('text')  // toggle clas bolasa ichiradi bolmasa qoshadi
-})
+// const change = document.getElementById('change')
 
-console.log(box1)
+// const box1 = document.getElementById('box1')
+
+// // box1.classList.add('text')  //class qoshish
+// // box1.classList.remove('text')   //classni ochirish
+
+
+// box1.addEventListener('click', () => {
+//     box1.classList.toggle('text')  // toggle clas bolasa ichiradi bolmasa qoshadi
+// })
+
+// console.log(box1)
+
+
